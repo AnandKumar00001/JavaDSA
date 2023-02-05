f73e3c3749246a92285548738e8489b055f21838
AnandKumar00001 <kumaranand.cse@gmail.com>
2023-02-05 13:29:19 +0530

    second

---
+++ first.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
